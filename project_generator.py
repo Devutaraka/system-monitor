b5fc69eadb68011cdfb83757a10e1ad969ae7291
Devutaraka <tarakadevu1@gmail.com>
2026-03-09 22:15:08 +0530

    Add project idea generator to project_generator.py

--- project_generator.py
+++ project_generator.py
@@ -1,1 +1,12 @@
+import random
 
+projects = [
+"Build a weather app",
+"Create a chatbot",
+"Make a todo list app",
+"Build a password manager",
+"Create a file organizer"
+]
+
+print("Project idea:")
+print(random.choice(projects))
