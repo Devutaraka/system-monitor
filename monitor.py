f3d6d7eba8d8b98f4513ae555da1e82550c3df32
Devutaraka <tarakadevu1@gmail.com>
2026-03-09 22:08:21 +0530

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,1 +1,14 @@
+import psutil
 
+# CPU usage
+cpu = psutil.cpu_percent(interval=1)
+
+# Memory usage
+memory = psutil.virtual_memory()
+
+# Disk usage
+disk = psutil.disk_usage('/')
+
+print("CPU Usage:", cpu, "%")
+print("Memory Usage:", memory.percent, "%")
+print("Disk Usage:", disk.percent, "%")
